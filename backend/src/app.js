7015b31117fcb68abad438316f30a2e1ee3bdcce
Aman Tiwari <tiwari95aman@gmail.com>
2025-06-13 16:39:59 +0530

    created a server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,16 @@
-console.log("this is first program");
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res)=>{
+ res.send("this is a home page");
+});
+app.use("/hello", (req,res)=> {
+   res.send("hello from the server");
+});
+app.use("/test", (req,res)=> {
+   res.send("hello from the server");
+});
+app.listen(7777,()=>{
+    console.log("server is listening on port 7777");
+});
