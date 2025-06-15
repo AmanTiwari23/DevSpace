f4f4c3fc0b27fc59c717f847dc00a1d63af000c8
Aman Tiwari <tiwari95aman@gmail.com>
2025-06-15 16:33:32 +0530

    passing multiple route handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,34 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user",(req,res,next)=> {
+  next();
+  console.log("1 responce")
+},(req,res,next)=>{
 
-app.get("/abc", (req, res) => {
-  console.log(req.params);
-  res.send({
-    firstname:"aman",
-    lastname:"tiwari"
-  });
+  next();
+   res.send("2nd responce");
+  console.log("2nd responce run");
+},(req,res)=> {
+  res.send("3rd responce");
+},(req,res)=>{
+  res.send("4 responce");
+})
 
-});
-app.post("/user", (req, res) => {
-  res.send("data has save successfully");
-});
-app.delete("/user", (req, res) => {
-  res.send("user deleted");
-});
+// app.get("/abc", (req, res) => {
+//   console.log(req.params);
+//   res.send({
+//     firstname:"aman",
+//     lastname:"tiwari"
+//   });
+
+// });
+// app.post("/user", (req, res) => {
+//   res.send("data has save successfully");
+// });
+// app.delete("/user", (req, res) => {
+//   res.send("user deleted");
+// });
 
 app.listen(7777, () => {
   console.log("server is listening on port 7777");
