2a0fdfa7fff1138c0b4af6de121cbd669ed15739
Aman Tiwari <tiwari95aman@gmail.com>
2025-06-18 16:28:47 +0530

    push data to the database using api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("../models/user");
 
 const app = express();
 
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Anuj",
+    lastName: "Tiwari",
+    emailId: "anuj@123.com",
+    password: "anuj123",
+  });
+try{
+   await user.save();
+  res.send("User added success fully");
+}catch (err){
+   res.status(400).send("Error saving data" + err.massage)
+}
+ 
+});
+
 connectDB()
   .then(() => {
-    console.log("database connection established...")
+    console.log("database connection established...");
     app.listen(7777, () => {
       console.log("server is listening on port 7777");
     });
