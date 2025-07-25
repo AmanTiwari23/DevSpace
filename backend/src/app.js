8b94c1af5ba691cfabf3c81c8ddad74f89bb9322
Aman Tiwari <tiwari95aman@gmail.com>
2025-07-25 15:46:15 +0530

    update user using api

--- backend/src/app.js
+++ backend/src/app.js
@@ -71,13 +71,13 @@ app.patch("/user",async (req,res)=>{
   const userId = req.body.userId;
   const data = req.body;
   try{
-    await User.findByIdAndUpdate({id: userId},data);
+    await User.findByIdAndUpdate({_id: userId},data);
     res.status(400).send("user updated");
   }catch(err){
    res.status(400).send("some thing wrong")
 
   }
-});
+}); 
 
 connectDB()
   .then(() => {
