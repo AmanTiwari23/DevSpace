14b045193778eacade9aa4d86cc86ae0d540c000
Aman Tiwari <tiwari95aman@gmail.com>
2025-07-29 13:41:50 +0530

    Data sanitaization and api level validation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,60 +1,78 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName : {
-        type : String,
-        required:true,
-        minLength:4,
-        maxLength:100
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
     },
-    lastName : {
-        type:String
-    },
-    city : {
-      type:  String
-    },
-    emailId :{
-      type:  String,
-      required:true,
-      unique:true,
-      lowercase:true,
-      trim:true
-
-    },
-    password:{
-      type:  String,
-      required:true,
-      minLength:4,
-        maxLength:10
-    },
-    age : {
-      type: Number,
-      min:18
-    },
-    gender:{
-      type:  String,
-      validate(value){
-        if(!["male","female","other"].includes(value)){
-            throw new Error("Gender data is not valid");
+    lastName: {
+      type: String,
+      validate(value) {
+        const invalidChars = ["@", "#", "$", "%", "&", "*"];
+        for (let char of value) {
+          if (invalidChars.includes(char)) {
+            throw new Error("Last name should not contain specail charactors");
+          }
         }
-      }
+      },
     },
-    photoUrl:{
-        type:String,
-        default:"https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
+    city: {
+      type: String,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!value.endsWith("@gmail.com")) {
+          throw new Error("not a valid email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 10,
+    },
+    age: {
+      type: Number,
+      validate(value) {
+        if (value < 18) {
+          throw new Error("not eligibale only 18+ allow");
+        }
+      },
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
     },
     about: {
-      type:String,
-      default:"this is the default about of the user"
-
+      type: String,
+      default: "this is the default about of the user",
     },
-    skills:{
-        type:[String],
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-    }
-},{
-    timestamps:true,
-});
-
-module.exports = mongoose.model("User",userSchema);
-
+module.exports = mongoose.model("User", userSchema);
--- backend/src/app.js
+++ backend/src/app.js
@@ -10,10 +10,14 @@ app.post("/signup", async (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
   try {
+    const Allow_user = []
+    if(data?.skills.length>10){
+      throw new Error("Skills can not be more than 10");
+    }
     await user.save();
     res.send("User added success fully");
   } catch (err) {
-    res.status(400).send("Error saving data :" +err );
+    res.status(400).send("Error saving data :" + err);
   }
 });
 
@@ -28,7 +32,7 @@ app.get("/user", async (req, res) => {
     } else {
       res.send(users);
     }
-  } catch (err) { 
+  } catch (err) {
     res.status(400).send("something went wrong");
   }
 });
@@ -57,15 +61,26 @@ app.delete("/user", async (req, res) => {
 app.patch("/user", async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
+
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if(!isUpdateAllowed){
+      throw new Error("update not allowed");
+    }
+    if(data?.skills.length>10){
+      throw new Error("Skills can not be more than 10");
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
-      runValidators:true
+      runValidators: true,
     });
     console.log(user);
     res.send("user updated");
   } catch (err) {
-    res.status(400).send("some thing wrong :"+err.message);
+    res.status(400).send("some thing wrong :" + err.message);
   }
 });
 
