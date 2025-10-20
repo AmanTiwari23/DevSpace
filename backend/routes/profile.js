bb1ee5f0ce4fad47fa5179f8c493fd045c3fa0b5
Aman Tiwari <tiwari95aman@gmail.com>
2025-10-20 15:38:53 +0530

    forget password api

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../src/middlewares/auth");
 const { validateEditProfileData } = require("../src/utils/validation");
+const bcrypt = require("bcrypt")
+const validator  = require("validator");
 
 const profileRouter = express.Router();
 
@@ -33,5 +35,37 @@ profileRouter.patch("/profile/edit",userAuth,async (req,res)=>{
    }
 
 });
+
+profileRouter.patch("/profile/forgetpass", userAuth, async(req,res)=>{
+    try{
+
+        const {oldPass,newPass} = req.body;
+
+        if(!oldPass || !newPass){
+            throw new Error("Both old and new passwords are required");
+        }
+        const user = req.user
+
+        const isMatch = await bcrypt.compare(oldPass, user.password);
+        if(!ismatch){
+            throw new Error("Old password is incorrect");
+        }
+
+        if(!validator.isStrongPassword(newPass)){
+            throw new Error("Enter a strong password");
+        }
+
+        const hashedPass = await bcrypt.hash(newPass,10);
+        user.password = hashedPass;
+        await user.save();
+
+        res.json({
+            message:"Password changed successfully",
+        });
+
+    }catch(err){
+        res.status(400).send("Error : "+ err.message);
+    }
+})
 
 module.exports  = profileRouter;
