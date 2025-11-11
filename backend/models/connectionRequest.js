e321c55b5d5e82e0cba278bba01e1b7595ada854
Aman Tiwari <tiwari95aman@gmail.com>
2025-11-12 01:05:11 +0530

    add checks only request sent to registerd users in db and user can not send requst to himself and make message dynamic when any requst is send

--- backend/models/connectionRequest.js
+++ backend/models/connectionRequest.js
@@ -1,24 +1,40 @@
 const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema({
-    fromUserId:{
-        type:mongoose.Schema.ObjectId,
-        required:true,
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
     },
-    toUserId:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    status:{
-        type:String,
-        enum:{
-            values:["ignored","interested","rejected"],
-            message:`{VALUE} is incorrect status type`,
-        },
+    status: {
+      type: String,
+      enum: {
+        values: ["ignored", "interested", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
     },
-},{timestamps:true});
+  },
+  { timestamps: true }
+);
 
+connectionRequestSchema.pre("save", function(next){
+    const connectionRequest = this;
+    //check if the from user id is same as touser id
 
-const ConnectionRequestModel  = new mongoose.model("ConnectionRequest",connectionRequestSchema);
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("cannot send request to yourself");
+    }
+    next();
+
+});
+
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
 
 module.exports = ConnectionRequestModel;
--- backend/routes/request.js
+++ backend/routes/request.js
@@ -15,7 +15,11 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req,res)=
     if(!allowedStatus.includes(status)){
       return res.status(400).json({message:"invalid status type: "+ status});
     }
-
+   
+    const toUser = await User.findById(toUserId);
+    if(!toUser){
+      return res.status(404).json({message:"User not found!"});
+    }
 
     const existingConnectionRequest = await ConnectionRequest.findOne({
       $or:[
@@ -37,7 +41,7 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req,res)=
     const data = await connectionRequest.save();
 
     res.json({
-      message:"Connection Request sent successfully!",
+      message:req.user.firstName+"is"+status+toUser.firstName,
       data,
     });
 
